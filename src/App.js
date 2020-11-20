13653075dd4cb15944f9d807225d557b304ff64e
Yousuf <josephali1993@gmail.com>
2020-11-20 20:08:24 +0400

    Change app fun to Class
    >> change app function to class
    +adding component state
    +use constractor to init state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
+import React from 'react'
 import MyAppHeader from './MyComponents/MyAppHeader'
 
-function App() {
-  return (
-    <div className="App">
-        
-        <MyAppHeader name={"Yousuf"}/>
+// create component class to use component state
 
-    </div>
-  );
+class App extends React.Component {
+
+  // constractor used for
+  //#1 setup and init local state
+  //#2 bind event handlers
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "Yousuf"
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <MyAppHeader name={this.state.name} />
+      </div>
+    );
+  }
 }
 
 export default App;
