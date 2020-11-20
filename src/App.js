58a30f0be20559c4fcab8aaa0d3e2107dbf3e5b6
Yousuf <josephali1993@gmail.com>
2020-11-20 15:30:10 +0400

    #Items and components
    #Items and components
    
    vid list:
    
    https://www.youtube.com/watch?v=oC63m_0MVV4&list=PLK-0XjrR8o7Ue1rguZAyQvFWIKrlxHZLh&index=5

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
        Meaw World!!!
-        </p>
+        </h1>
         <p>
           ;)
         </p>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,56 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const MyFirstComponent = (props) => {
+
+  return (<p>This is My MyFirstComponent!!! \ {props.name} / </p>)
+}
+
+const MySecondComponent = ({ name }) => {
+
+  return (<p>Value passed is \ {name} / </p>)
+}
+
+const item = <h3>Huh</h3>
+
+
+const MyThirdComponent = ({ n1, n2 }) => {
+
+  return (<div>
+    {item}
+    <MyFirstComponent name={n1} />
+    <MySecondComponent name={n2}/>
+  </div>)
+}
+
+
+const element =
+  <div>
+    <h2>Mewo element!!!</h2>
+    <h5>meaw meaw</h5>
+    {item}
+    <MyFirstComponent name={'MEAW'} />
+    <MyFirstComponent name={'neaw'} />
+    <MyFirstComponent name={'mrrawo'} />
+    <MySecondComponent name={'Snow'} />
+
+<br/>
+<br/>
+<br/>
+
+<MyThirdComponent n1={'hello'} n2={'world'}/>
+
+  </div>
+
+
+
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  /*<React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>*/element,
   document.getElementById('root')
 );
 
